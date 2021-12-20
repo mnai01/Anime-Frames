8d09ec2da1d6cb5d8cd86d8f30f2976630da5da4
Ian M <44310933+mnai01@users.noreply.github.com>
2021-12-19 19:39:48 -0500

    Color Thief Implementation

--- index.py
+++ index.py
@@ -1,11 +1,9 @@
-import time
 import cv2
 from colorthief import ColorThief
-import numpy as np
 
 
 def getAverageColorOfVideo():
-    # get image
+    # Get image
     vidcap = cv2.VideoCapture('OnePiece.mp4')
     success, image = vidcap.read()
     count = 0
@@ -15,57 +13,35 @@ def getAverageColorOfVideo():
     f.seek(0)
     f.truncate()
 
-    # duration = vidcap.get(cv2.CAP_PROP_POS_MSEC)
     frame_count = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
     total = frame_count / 24
-    x = total / 100
     seconds = 0
 
     while success:
-        # resize image (for optimization)
-        # output = cv2.resize(image, (100, 100))
-        # save frame as JPEG file
+        # Save frame as JPEG file
         cv2.imwrite("frame.jpg", image)
 
-        # print('CALCULATING COLOR...')
-        # color_thief = ColorThief('frame.jpg')
+        # Init color theif with frame
+        color_thief = ColorThief('frame.jpg')
 
-        # dominant_color = color_thief.get_color(quality=10)
-        # print(dominant_color)
-        # f.write(str(dominant_color[0]) + ","
-        #         + str(dominant_color[1]) + ","
-        #         + str(dominant_color[2]) + "\n")
+        # Find dominant color
+        dominant_color = color_thief.get_color(quality=10)
+        print(dominant_color)
+        r, g, b = str(dominant_color[0]), str(
+            dominant_color[1]), str(dominant_color[2])
 
-        # get image
-        success, image = vidcap.read()
-        # print('Read a new frame: ', success)
-
-        img = cv2.imread("frame.jpg", cv2.IMREAD_UNCHANGED)
-        data = np.reshape(img, (-1, 3))
-        print(data.shape)
-        data = np.float32(data)
-
-        criteria = (cv2.TERM_CRITERIA_EPS +
-                    cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-        flags = cv2.KMEANS_RANDOM_CENTERS
-        compactness, labels, centers = cv2.kmeans(
-            data, 1, None, criteria, 10, flags)
-
-        b, g, r = centers[0].astype(np.int32)
-
-        f.write(str(r) + ","
-                + str(g) + ","
-                + str(b) + "\n")
-
+        # Write rgb to file
+        f.write(r + "," + g + "," + b + "\n")
         print(r, g, b)
         count += 1
 
+        # Calculate Progress of frames left
         if((count % 24) == 0):
             seconds = (count / 24)
             print('TOTAL:', (seconds / total)*100)
 
-            # print(seconds / total)
-            # print(round(seconds / total, 1))
+        # get image
+        success, image = vidcap.read()
 
     f.close()
     # OnePiece.mp4
