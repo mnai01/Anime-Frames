007a65a2e5afc427e5355982b68428564b82f086
Ian M <44310933+mnai01@users.noreply.github.com>
2021-12-19 19:35:16 -0500

    OpenCV implementation

--- index.py
+++ index.py
@@ -21,38 +21,25 @@ def getAverageColorOfVideo():
     x = total / 100
     seconds = 0
 
-    while success:
-        # resize image (for optimization)
-        # output = cv2.resize(image, (100, 100))
+    while count != frame_count:
         # save frame as JPEG file
         cv2.imwrite("frame.jpg", image)
 
-        # print('CALCULATING COLOR...')
-        # color_thief = ColorThief('frame.jpg')
+        print('CALCULATING COLOR...')
 
-        # dominant_color = color_thief.get_color(quality=10)
-        # print(dominant_color)
-        # f.write(str(dominant_color[0]) + ","
-        #         + str(dominant_color[1]) + ","
-        #         + str(dominant_color[2]) + "\n")
-
-        # get image
-        success, image = vidcap.read()
-        # print('Read a new frame: ', success)
-
+        # Get Dominate color in scene
         img = cv2.imread("frame.jpg", cv2.IMREAD_UNCHANGED)
         data = np.reshape(img, (-1, 3))
         print(data.shape)
         data = np.float32(data)
-
         criteria = (cv2.TERM_CRITERIA_EPS +
                     cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
         flags = cv2.KMEANS_RANDOM_CENTERS
         compactness, labels, centers = cv2.kmeans(
             data, 1, None, criteria, 10, flags)
-
         b, g, r = centers[0].astype(np.int32)
 
+        # Write RGB to file
         f.write(str(r) + ","
                 + str(g) + ","
                 + str(b) + "\n")
@@ -60,15 +47,15 @@ def getAverageColorOfVideo():
         print(r, g, b)
         count += 1
 
+        # Calculate Progress of frames left
         if((count % 24) == 0):
             seconds = (count / 24)
             print('TOTAL:', (seconds / total)*100)
 
-            # print(seconds / total)
-            # print(round(seconds / total, 1))
+        # Get next frame
+        success, image = vidcap.read()
 
     f.close()
-    # OnePiece.mp4
 
 
 getAverageColorOfVideo()
